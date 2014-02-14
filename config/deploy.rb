a6f0a771ac72057568157172e0d8b5a95c2c0d66
Javier <javierhonduco@gmail.com>
2014-02-14 22:30:09 +0100

    Capistrano added

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+require 'bundler/capistrano'
+set :application, "gul-talks"
+
+set :scm, :none
+set :repository, "."
+set :deploy_via, :copy
+
+set :deploy_to, "/home/gultalks/test"
+
+set :use_sudo, false
+
+
+set :user, "gultalks"
+
+role :web, "163.117.156.71"                         # Your HTTP server, Apache/etc
+role :db, "163.117.156.71", :primary=> true
+after "deploy", "deploy:restart" 
+
+namespace :deploy do
+    task :restart, :roles => :web do
+        run "touch #{deploy_to}/current/tmp/restart.txt"
+    end
+end
